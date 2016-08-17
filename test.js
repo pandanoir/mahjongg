b733f1918b1617053787ca5e712679a0740d1500
Naoto Ikuno <moon_in_france@yahoo.co.jp>
2016-08-17 17:59:07 +0900

    rewrite test.js

--- test.js
+++ test.js
@@ -1,33 +1,49 @@
-var tiles = require('./tiles.js');
-var yaku = require('./yaku.js');
+const tiles = require('./tiles.js');
+const yaku = require('./yaku.js');
 
-var bambooSuits = tiles.bambooSuits;
-var circleSuits = tiles.circleSuits;
-var characterSuits = tiles.characterSuits;
-var honorTiles = tiles.honorTiles;
-var Hand = tiles.Hand;
+const s = tiles.bambooSuits;
+const m = tiles.characterSuits;
+const p = tiles.circleSuits;
 
-// console.log(yaku.isAllSimples(new Hand([
-//     bambooSuits[2], bambooSuits[3], bambooSuits[4],
-//     bambooSuits[3], bambooSuits[4], bambooSuits[5],
-//     circleSuits[5], circleSuits[6], circleSuits[7],
-//     characterSuits[6], characterSuits[7], characterSuits[8],
-//     bambooSuits[6], bambooSuits[6]
-// ])));
-// console.log(!yaku.isAllSimples(new Hand([
-//     bambooSuits[3], bambooSuits[3], bambooSuits[4],
-//     bambooSuits[3], bambooSuits[4], bambooSuits[5],
-//     circleSuits[5], circleSuits[6], circleSuits[7],
-//     characterSuits[6], characterSuits[7], characterSuits[8],
-//     bambooSuits[6], bambooSuits[6]
-// ])));
+const bambooSuits = tiles.bambooSuits;
+const characterSuits = tiles.characterSuits;
+const circleSuits = tiles.circleSuits;
 
+const honorTiles = tiles.honorTiles;
+const Hand = tiles.Hand;
 
+const testHands = [
+    [s[1], s[1], s[1], s[3], s[3], s[3], m[5], m[5], m[5], p[3], p[4], p[5], p[9], p[9]],
+    [m[2], m[2], m[5], m[5], m[5], s[6], s[7], s[8], p[2], p[3], p[4], p[7], p[7], p[7]],
+    [m[3], m[3], m[4], m[4], m[5], m[5], p[2], p[2], p[3], p[3], p[4], s[9], s[9], p[4]],
+].map(item => new Hand(item));
 
-// console.log(JSON.stringify(new Hand([
-//     bambooSuits[2], bambooSuits[3], bambooSuits[4],
-//     bambooSuits[2], bambooSuits[3], bambooSuits[4],
-//     bambooSuits[2], bambooSuits[3], bambooSuits[4],
-//     characterSuits[2], characterSuits[2], characterSuits[2],
-//     bambooSuits[6], bambooSuits[6]
-// ]).getPairs(), null, '  '));
+testHands.forEach(testHand => {
+    console.log(testHand.hand.map(tile => tileToString(tile)).join(' '));
+    console.log(showYaku(testHand));
+});
+
+function showYaku(hand) {
+    const yakus = yaku.getYaku(hand);
+    const yakuList　= new Map([
+        ['断么九', 'isAllSimples'],
+        ['一盃口', 'isOneSetOfIdenticalSequences'],
+        ['役牌', 'isHonorTiles'],
+        ['三色同順', 'isThreeColourStraight'],
+        ['混全帯么九', 'isTerminalOrHonorInEachSet'],
+        ['一気通貫', 'isStraight'],
+        ['三色同刻', 'isThreeColourTriplets'],
+        ['三暗刻', 'isThreeClosedTriplets'],
+        ['二盃口', 'isTwoSetOfIdenticalSequences'],
+        ['七対子', 'isSevenPairs'],
+        ['国士無双', 'isThirteenOrphans']
+    ]);
+    const res = [];
+    for (let i = 0, _i = yakus.length; i < _i; i++) {
+        res.push([]);
+        for (const [key, value] of yakuList) {
+            if (yakus[i][value]) res[i].push(key);
+        }
+    }
+    return res;
+}
